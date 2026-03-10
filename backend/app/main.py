c9a10d1c45951a6f1398d9eff2c48c9bc1863361
sofiarm27 <sofima0142@gmail.com>
2026-03-10 12:17:16 -0500

    permisos para vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
         "http://[::1]:5174",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://lex-contract-final.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
--- public/services.js
+++ public/services.js
@@ -1,3 +1,5 @@
 //export const API_URL = "http://localhost:8000";
 
-export const API_URL = "https://lexcontract-final.onrender.com";
+//export const API_URL = "https://lexcontract-final.onrender.com";
+
+export const API_URL = import.meta.env.VITE_API_URL || "https://lexcontract-final.onrender.com";
